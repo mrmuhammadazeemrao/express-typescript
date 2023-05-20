69b88535f9daaa659223ddb0c59520ad8b032bd8
Muhammad Azeem Rao <mrmuhammadazeemrao@gmail.com>
2023-05-20 00:30:39 -0700

    Add apis, migrations and db configurations

--- database.ts
+++ database.ts
@@ -9,4 +9,5 @@ sequelize.authenticate()
   .then(() => console.log('Connection has been established successfully.'))
   .catch((error) => console.error('Unable to connect to the database:', error));
 
+
 export default sequelize;
--- index.ts
+++ index.ts
@@ -1,10 +1,14 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import blogRoutes from './routes/blogRoutes';
+import sequelize from './database';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
+
+app.use('/api', blogRoutes);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
@@ -12,4 +16,8 @@ app.get('/', (req: Request, res: Response) => {
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
+
+app.get('/api/database', (req: Request, res: Response) => {
+  res.send(`Connected to database: ${sequelize.config.database}`);
 });
